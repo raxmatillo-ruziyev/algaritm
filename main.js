b1634774fc1f41d71e9e53b48c85d052f2b69112
raxmatillo-ruziyev <raxmatillo248@gmail.com>
2024-04-03 19:18:33 +0500

    add

---
+++ main.js
@@ -0,0 +1,72 @@
+//  let  a = prompt("sonni kiriting ")-0
+//   if (a%3==0) {
+//    console.log("Fizz");    
+//   } else if(a%5==0) {
+//   console.log("Buzz");
+//   }
+//   else if (a%3==0 && a%5==0) {
+//     console.log("FizzBuzz");
+//   }
+//   else{
+//     console.log("siz adashdingiz");
+//   }
+// 2-masala
+// let  a = prompt("sonni kiriting ")-0
+// let  b = prompt("sonni kiriting ")-0
+// let  c = prompt("sonni kiriting ")-0
+// if ((a+b+c)%3==0) {
+//     console.log((a+b+c)/3);
+// } else {
+//     console.log(Math.floor(((a+b+c)/3)));
+// }
+// 3-masala
+
+
+// 4-masala
+// let a=[1,3,5,2,4]
+// if ((a[0]>a[1])&&(a[0]>a[2])&&(a[0]>a[3])&&(a[0]>a[4])) {
+//     console.log(a[0]);
+// } else  if (((a[1]>a[2])&&(a[1]>a[3])&&(a[1]>a[4]))) {
+//     console.log(a[1]);
+// }else if(((a[2]>a[3])&&(a[2]>a[4]))){
+//     console.log(a[2]);
+// }else{
+//     console.log(a[4]);
+// }
+// 5-masala
+// a = prompt("sonni kiriting ")-0
+// if ((a>0)&& a%Math.sqrt(a)==Math.sqrt(a)) {
+//     console.log(Math.sqrt(a));
+// } else {
+//     console.log("bu sondan ildiz chiqmaydi");
+// }
+// 6-masala
+// let a=[8,5,3,2,7,18]
+// let res = son.reverse()
+// console.log(res);
+// // 8-masala
+// a = prompt("sonni kiriting ")-0
+// b = prompt("amalni kirit ")
+// c = prompt("sonni kiriting ")-0
+// if( b ==="+"){
+//  console.log(a+c);
+// }
+// else if (b== "*") {
+//     console.log(a*c);
+// }
+// else{
+//     console.log("xatolik bor");
+// }
+// // 7-masala
+// function myapp(a){
+//  let res =[];
+//  for (let i = 0; i <= a; i++) {
+//     if(a%i===0){
+//         res.push(i)
+//     }
+//   return res;
+    
+//  }
+// }
+//  a= 12;
+//  console.log();
